c82aefd02f6f21a0ec8e292d898d0b93759002bf
txya900619 <wayne900619@gmail.com>
2022-04-18 16:57:49 +0800

    add test_month for base rnn lm

---
+++ recipes/MATBN/LM/test_month.py
@@ -0,0 +1,81 @@
+import sys
+
+import torch
+import speechbrain as sb
+from speechbrain.dataio import dataset
+from speechbrain.utils.distributed import run_on_main
+from hyperpyyaml import load_hyperpyyaml
+from train import LM
+import glob
+import os
+import pandas as pd
+
+
+def dataio_prepare(hparams):
+    @sb.utils.data_pipeline.takes("transcription")
+    @sb.utils.data_pipeline.provides(
+        "transcription", "tokens_bos", "tokens_eos"
+    )
+    def transcription_pipline(transcription):
+        yield transcription
+        tokens_list = hparams["tokenizer"].encode_as_ids(transcription)
+        tokens_bos = torch.LongTensor([hparams["bos_index"]] + (tokens_list))
+        yield tokens_bos
+        tokens_eos = torch.LongTensor(tokens_list + [hparams["eos_index"]])
+        yield tokens_eos
+
+    data_folder = "results/prepare_cna_month"
+    datasets = {}
+    for json_file_path in glob.glob(os.path.join(data_folder, "*.json")):
+        json_file_name = os.path.basename(json_file_path).replace(".json", "")
+        datasets[json_file_name] = dataset.DynamicItemDataset.from_json(
+            json_path=json_file_path,
+            replacements={"data_root": data_folder},
+            dynamic_items=[transcription_pipline],
+            output_keys=["transcription", "tokens_bos", "tokens_eos"],
+        )
+
+    return datasets
+
+
+if __name__ == "__main__":
+    hparams_file_path, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+    with open(hparams_file_path) as hparams_file:
+        hparams = load_hyperpyyaml(hparams_file, overrides)
+
+    sb.utils.distributed.ddp_init_group(run_opts)
+
+    sb.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file_path,
+        overrides=overrides,
+    )
+
+    run_on_main(hparams["pretrainer"].collect_files)
+    hparams["pretrainer"].load_collected(device=run_opts["device"])
+
+    datasets = dataio_prepare(hparams)
+
+    lm_brain = LM(
+        modules=hparams["modules"],
+        opt_class=hparams["optimizer"],
+        hparams=hparams,
+        run_opts=run_opts,
+        checkpointer=hparams["checkpointer"],
+    )
+
+    months = []
+    loss = []
+
+    for month_name in sorted(datasets.keys()):
+
+        month_loss = lm_brain.evaluate(
+            datasets[month_name],
+            min_key="loss",
+            test_loader_kwargs=hparams["test_dataloader_opts"],
+        )
+        months.append(month_name)
+        loss.append(month_loss)
+    pd.DataFrame({"month": months, "loss": loss}).to_csv(
+        os.path.join(hparams["output_folder"], "month_loss.csv"), index=False
+    )
